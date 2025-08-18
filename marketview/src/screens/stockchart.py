c70fa86e05e81fdb7c893352e59321347902be4c
pforderique <pforderique@gmail.com>
2025-08-17 21:43:35 -0700

    fix the symbol bug

--- marketview/src/screens/stockchart.py
+++ marketview/src/screens/stockchart.py
@@ -1,19 +1,27 @@
+"""Stock Chart Page"""
 import curses
-import npyscreen
 import os
+
+import npyscreen
 
 from src import common
 from src.screens import chart
 
 
 class StockChartScreen(npyscreen.FormBaseNew):
+    """Stock chart screen for displaying stock charts."""
+
     def create(self):
+        """Create the stock chart screen."""
         self.symbol = None
-        self.uh_oh_widget = self.add(npyscreen.FixedText, value="Uh-oh! No stock was passed.", rely=2, relx=2)
-        self.back_button = self.add(npyscreen.ButtonPress, name="Back", rely=-3, relx=2)
+        self.uh_oh_widget = self.add(
+            npyscreen.FixedText, value="Uh-oh! No stock was passed.", rely=2, relx=2)
+        self.back_button = self.add(
+            npyscreen.ButtonPress, name="Back", rely=-3, relx=2)
         self.back_button.whenPressed = self.on_back
 
     def set_symbol(self, symbol):
+        """Set the stock symbol for the chart and enter page."""
         self.symbol = symbol
         chart_screen = chart.ChartScreen(self.symbol)
         cb = chart_screen.on_transaction
@@ -22,9 +30,10 @@ class StockChartScreen(npyscreen.FormBaseNew):
         curses.endwin()
         os.system("clear")
         chart_screen.show()
-        input("Press [Enter] to return...")
+        input("Press [Enter!] to return...")
         self.parentApp.txn_listener.remove_callback(cb)
-        self.parentApp.run()
+        self.parentApp.switchForm(common.Screen.SEARCH)
 
     def on_back(self):
+        """Handle the back action."""
         self.parentApp.switchForm(common.Screen.SEARCH)
